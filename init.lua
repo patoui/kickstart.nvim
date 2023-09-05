39f4a3cfc23e7291c9f5ad7df4ae96a6d06dd7bb
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-09-05 10:22:15 -0400

    chore: added vertical rulers

--- init.lua
+++ init.lua
@@ -226,6 +226,9 @@ vim.o.smartcase = true
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
 
+-- Vertical rulers
+vim.o.colorcolumn = '80,120'
+
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeout = true
